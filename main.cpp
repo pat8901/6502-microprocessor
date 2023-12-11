33c232cf2ab285941ad1bfd7f9c91bf022780d0a
Patrick <95184328+pat8901@users.noreply.github.com>
2023-12-10 22:04:41 -0500

    Added reset function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,45 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(){
+struct CPU
+{
+    using Byte = unsigned char;
+    using Word = unsigned short;
+
+    Word PC; // Program counter 
+    Word SP; // Stack pointer
+
+    Byte Accumulator, x, y; // Registers
+    
+    // Bit field, each flag is 1 bit. This makes up the Processor Status Register
+    Byte C : 1; // Carry Flag   
+    Byte Z : 1; // Zero Flag 
+    Byte I : 1; // Interrupt Flag 
+    Byte D : 1; // Decimal Flag 
+    Byte B : 1; // Break Flag 
+    Byte V : 1; // Overflow Flag 
+    Byte N : 1; // Negative Flag 
+
+    //  non-maskable interrupt
+    void NMI(){
+        SP = PC & 0xFF;  // push low byte PC onto SP
+        SP = (PC>>8) & 0xFF; // push high byte PC onto SP
+        // processor status onto SP
+        // disable interupts
+        PC = 0xFFFA;
+    }
+    // Cold reset
+    void Reset(){
+        PC = 0xFFFC;
+        D = 0;
+    }
+};
+
+
+int main()
+{
+    CPU cpu;
+    cpu.Reset();
     printf("Hello World!");
     return 0;
 }
